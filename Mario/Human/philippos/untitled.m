7cea6abbb46dbdf39a046ddc9b937a01b62657ea
Paris Mavromoustakos <parismav87@gmail.com>
2014-05-13 15:01:05 +0200

    stuff

--- Mario/Human/philippos/untitled.m
+++ Mario/Human/philippos/untitled.m
@@ -1,22 +1,23 @@
-file = fopen('difficulties.txt');
-A=fscanf(file,'%f %f',[5 inf]);
-A=A';
-A=A(:,:)
+% file = fopen('difficulties.txt');
+% A=fscanf(file,'%f %f',[5 inf]);
+% A=A';
+% A=A(:,:)
 
+
+% figure()
+% plot(A);
+% legend('STRAIGHT','HILLS','TUBES','JUMPS','CANNONS','Location','northwest');
+% xlabel('Time (Number of iterations)');
+% ylabel('Difficulty levels');
+% saveTightFigure('difficulties.png')
+
+file = fopen('section4Emotions.txt');
+b=fscanf(file,'%f %f',[7 inf]);
+b=b';
 
 figure()
-plot(A);
-legend('STRAIGHT','HILLS','TUBES','JUMPS','CANNONS','Location','northwest');
-xlabel('No. of measurements');
-ylabel('Difficulty levels');
-saveTightFigure('difficulties.png')
-
-% file = fopen('allEmotions.txt');
-% b=fscanf(file,'%f %f',[7 inf]);
-% b=b';
-% 
-% figure()
-% plot(b);
-% legend('neutral','happy','surprised','angry','disgusted','afraid','sad');
-% xlabel('No. of frames recorded');
-% ylabel('Emotion probability estimates');
+plot(b);
+legend('neutral','happy','surprised','angry','disgusted','afraid','sad');
+xlabel('Time (Number of frames)');
+ylabel('Emotion probability estimates');
+saveTightFigure('section4Emotions.png')
