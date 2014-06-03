710c9cd49785b5295c06a3433eaed9073282ebd7
Arjen Swellengrebel <arjen_swellengrebel@msn.com>
2014-06-03 18:31:41 +0200

    Random stuff I never committed?

--- BayesOptMario/nbproject/Makefile-variables.mk
+++ BayesOptMario/nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=None-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/None-Windows
 CND_ARTIFACT_NAME_Debug=bayesoptmario
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/bayesoptmario
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/None-Windows/bayesoptmario
+CND_PACKAGE_DIR_Debug=dist/Debug/None-Windows/package
 CND_PACKAGE_NAME_Debug=bayesoptmario.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/bayesoptmario.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/None-Windows/package/bayesoptmario.tar
 # Release configuration
 CND_PLATFORM_Release=MinGW-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
